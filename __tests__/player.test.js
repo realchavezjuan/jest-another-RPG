04ebdcde2808b92158cb4f095627c026be1862ea
Juan Chavez <juan@Juans-Air-12.home>
2022-01-13 12:49:38 -0800

    prototype methods

--- __tests__/player.test.js
+++ __tests__/player.test.js
@@ -5,8 +5,7 @@ jest.mock('../lib/Potion');
 console.log(new Potion());
 
 const Player = require('../lib/Player');
-const { expect } = require('@jest/globals');
-const { object } = require('webidl-conversions');
+
 
 test('creates a player object', () => {
 
@@ -19,4 +18,22 @@ test('creates a player object', () => {
     expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]));
 
 
+});
+
+test('gets players stats as an object', () => {
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory form player or returns false',() => {
+    const player = new Player('Dave');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+    expect(player.getInventory()).toEqual(false);
 });
